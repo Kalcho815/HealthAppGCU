b3869e3618e318ca53ebf86dd72dafcf532f4a52
kaluber <kombat1511>
2021-04-30 13:41:47 +0300

    Added Username input field for the Regsiter and made changes to Index page

--- HealthAppGCU/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ HealthAppGCU/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -49,6 +49,10 @@ namespace HealthAppGCU.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -72,7 +76,7 @@ namespace HealthAppGCU.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                var user = new User { UserName = Input.Username, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
